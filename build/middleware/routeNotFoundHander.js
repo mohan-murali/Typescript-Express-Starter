35fc5a93cd51c87672b4ed7144d2bc80cdb1613f
Mohan Murali <mohanmuralid28@gmail.com>
2021-11-05 19:49:46 +0530

    added built artifacts in git ignore

--- build/middleware/routeNotFoundHander.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routeNotFoundHander = void 0;
-const routeNotFoundHander = (req, res, next) => {
-    res.status(404).json({ success: false, message: "No route found" });
-};
-exports.routeNotFoundHander = routeNotFoundHander;
