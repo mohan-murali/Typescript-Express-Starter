85fd9b21b782f3372075a78f559c0d59c26d54e0
Mohan Murali <mohanmuralid28@gmail.com>
2021-11-08 09:51:13 +0530

    mongo db conneciton not working

---
+++ src/config/dbConfig.ts
@@ -0,0 +1,13 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => {
+//   const dbUri = process.env.MONGO_URI || "";
+//   console.log(dbUri);
+//   try {
+//     await mongoose.connect(dbUri);
+//     console.log("Mongo DB connected ...");
+//   } catch (err: any) {
+//     console.error("error is", err.message);
+//     process.exit(1);
+//   }
+// };
