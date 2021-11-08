a80d9945b878120a85e99ad41dcf91f3ae617a3e
Mohan Murali <mohanmuralid28@gmail.com>
2021-11-08 13:50:34 +0530

    removed unused request, response objects

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import * as dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cookieSession from "cookie-session";
